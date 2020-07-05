b379fb2a21dd0290b4eaed78e301790149427a56
Sachin Gupta <sachingupta@Sachins-MacBook-Pro.local>
2020-07-04 22:57:55 -0500

    Initial commit

---
+++ src/test/java/com/guptasachinn/personal/SachinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guptasachinn.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SachinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
